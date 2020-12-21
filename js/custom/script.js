999ff106a12e2422be7ad86cbd7d95f6a0875923
sigitfrank <sigittt53@gmail.com>
2020-12-21 15:46:29 +0700

    rps to portofolio

---
+++ js/custom/script.js
@@ -0,0 +1,12 @@
+function type(element, text) {
+    new TypeIt("#" + element, {
+        strings: text,
+        speed: 120
+    }).go();
+}
+
+type('myName',' <span class="text-warning">echo</span> "Sigit"');
+
+setTimeout(() => {
+    document.getElementById('myName').innerHTML = "Si<span class='name'>git</span>";
+}, 3000);
